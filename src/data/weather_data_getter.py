22144ef56fbe09f187a2e6015ec42d67433072ff
Farouk <ferouk.ferjani@tum.de>
2021-06-22 11:47:09 +0200

    updated weather getter class

--- src/data/weather_data_getter.py
+++ src/data/weather_data_getter.py
@@ -4,10 +4,11 @@ import pandas as pd
 from datetime import datetime
 from meteostat import Point, Hourly
 from geopy.geocoders import Nominatim
+from pathlib import Path
 from data_getter import BaseDataGetter
 
 
-class WeatherDataGetter():
+class WeatherDataGetter(BaseDataGetter):
     """
     This class is responsible for downloading the Weather data.
     This data contains Temperature(°C), Precipitation(mm), Wind Speed(km/h),
@@ -18,7 +19,7 @@ class WeatherDataGetter():
         Constructor for the Weather Data Getter
         """
         BaseDataGetter.__init__(self, name=name)
-        geolocator = Nominatim(user_agent="weather")      
+        geolocator = Nominatim(user_agent="weather_agent")      
         self.location = city
         self.latitude = geolocator.geocode(city).latitude
         self.longitude = geolocator.geocode(city).longitude        
@@ -30,20 +31,19 @@ class WeatherDataGetter():
         """
         self.start_date = datetime.strptime(start_date, '%Y-%m-%d')         
         self.end_date = datetime.strptime(end_date, '%Y-%m-%d')
-        position = Point(self.latitude, self.longitude)
-
-        if overwrite or not os.path.exists(os.path.join(self.data_dir, 'weather_data.csv')):                    
-            data = Hourly(position, start_date, end_date)
-            data = data.fetch()
+        position = Point(self.latitude, self.longitude)                            
+        data = Hourly(position, self.start_date, self.end_date)
+        data = data.fetch()
         
         return data
 
-    def save_weather_data(self, data):        
+    def save_weather_data(self, data, overwrite=False):        
         """
         Saves the dataframe to a csv file
         """
-        # Save DataFrame to csv
-        data.to_csv(os.path.join(self.data_dir, 'weather_data.csv'), index=False)
+        if overwrite or not os.path.exists(os.path.join(self.data_dir, 'weather_data.csv')):
+            # Save DataFrame to csv
+            data.to_csv(os.path.join(self.data_dir, 'weather_data.csv'))
         
         return
 
@@ -52,10 +52,11 @@ class WeatherDataGetter():
 
     def _process_raw_data():
         raise NotImplementedError
-
+        
 
 
    
 if __name__ == '__main__':
     weatherguy = WeatherDataGetter()
-    weatherguy.get_weather_data()
+    df = weatherguy.get_weather_data(start_date='2020-01-01')
+    weatherguy.save_weather_data(data=df)
