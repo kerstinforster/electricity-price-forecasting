a8232c78c733ed5ae66341d58697750fcfec533c
Ferouk <ferouk.ferjani@tum.de>
2021-07-04 00:18:02 +0200

    weather data getter integrated in base

--- src/data/weather_data_getter.py
+++ src/data/weather_data_getter.py
@@ -18,68 +18,53 @@ class WeatherDataGetter(BaseDataGetter):
         """
         Constructor for the Weather Data Getter
         """
-        BaseDataGetter.__init__(self, name=name)
+        # BaseDataGetter.__init__(self, name=name)
+        super().__init__(name)
         geolocator = Nominatim(user_agent="weather_agent")      
         self.location = location
         self.latitude = geolocator.geocode(location).latitude
         self.longitude = geolocator.geocode(location).longitude
 
-    
-
-    def get_weather_data(self, start_date='2012-01-01', end_date='2021-06-21'):       
-        """
-        fetches the data from meteostat
-        date must be in format 'YYYY-MM-DD'        
-        return DataFrame        
-        """
-        self.start_date = start_date         
-        self.end_date = end_date
-        # self.timezone = timezone
-        position = Point(self.latitude, self.longitude)
-        # Get all the data point until the last hour of the last day
-        end_date = end_date + ' ' + '23:59'                            
-        data = Hourly(loc=position, start=datetime.strptime(start_date, '%Y-%m-%d'), \
-            end=datetime.strptime(end_date, '%Y-%m-%d %H:%M'))
-        # Check data coverage
-        self.check_data_coverage(data)        
-        # Access the resulting DataFrame
-        data = data.fetch()
-        
-        return data
-
-    def save_weather_data(self, data, overwrite=False):        
-        """
-        Saves the dataframe to a csv file in 
-        """
-        if overwrite or not os.path.exists(os.path.join(self.data_dir, 'weather_data.csv')):
-            # Save DataFrame to csv
-            data.to_csv(os.path.join(self.data_dir, 'weather_data.csv'))
-        
-        return       
 
     def check_data_coverage(self, data):
         """
         return Integer between 0 (no records) and 1 (all records)
         """
-        return print("Data coverage of this period is   {0:.0%}".format(data.coverage()))       
+        print("Data coverage {0:.0%}".format(data.coverage()))
+        pass      
     
 
-    def _get_raw_data():
+    def _get_raw_data(self):
         """
-        Abtract function from base class not needed here
+        This function queries hourly weather data for
+        a single geographical point.
+        :return: meteostat.interface.hourly.Hourly object
         """
-        raise NotImplementedError
+        position = Point(self.latitude, self.longitude)
+        # Get all the data point until the last hour of the last day
+        end_date = self.end_date + ' ' + '23:59'
+        data = Hourly(loc=position, start=datetime.strptime(self.start_date, '%Y-%m-%d'), \
+            end=datetime.strptime(end_date, '%Y-%m-%d %H:%M'))        
+        # Check data coverage
+        self.check_data_coverage(data)
 
-    def _process_raw_data():
+        return data
+
+    def _process_raw_data(self, data):
         """
-        Abtract function from base class not needed here
+        This function gives access to the resulting DataFrame of the query
+        :return: Pandas DataFrame
         """
-        raise NotImplementedError
+        fp_data = data.fetch()
+        fp_data.index.name = "Time"
+        fp_data.reset_index(level=0, inplace=True)
+        
+
+        return fp_data
         
 
 
    
-if __name__ == '__main__':
-    weatherguy = WeatherDataGetter()
-    df = weatherguy.get_weather_data()
-    weatherguy.save_weather_data(data=df)
+if __name__ == '__main__':    
+    wg = WeatherDataGetter()
+    wg.get_data()
