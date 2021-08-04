8b210a97268d90a9a8eb4e250147085a2f4e530f
Florian Mysliwetz <ga63let@mytum.de>
2021-08-04 20:22:48 +0200

    Added documentation to LinearRegressionModel.

--- src/models/linear_regression_model.py
+++ src/models/linear_regression_model.py
@@ -23,11 +23,25 @@ class LinearRegressionModel(BaseModel):
 
     def train(self, x_train: np.array, y_train: np.array, model_params: dict,
               save_at: str = None) -> Any:
+        """
+        Trains a model with the provided data (x_train, y_train)
+        :param x_train: np.array with (n_timesteps X n_features X n_step_size)
+        :param y_train: np.array with (n_timesteps X n_labels)
+        :param model_params: dictionary which sets the relevant hyperparameters
+        :param save_at: saves model at given path with given name if not None
+        :return: trained instance of the model
+        """
         # TODO: check if this algo also takes multivariate regression tasks
         self.model.fit(x_train, y_train)
         self.model_trained = True
 
     def predict(self, x_input: np.array) -> np.array:
+        """
+        Uses the trained model to make a prediction based on x_input
+        :param x_input: single timestep with n_features X n_step_size
+        :return: Correctly timestamped pandas dataframe with the predicted
+        value/s
+        """
         if self.model_trained:
             return self.model.predict(x_input)
         else:
@@ -35,6 +49,10 @@ class LinearRegressionModel(BaseModel):
                              "loaded before predictions can be made.")
 
     def save(self, path: str):
+        """
+        Saves the model at the given path with the given name
+        :param path: path and model name in modelzoo
+        """
         if self.model_trained:
             # TODO: fix path to modelzoo here
             pkl.dump(self.model, path)
@@ -43,6 +61,10 @@ class LinearRegressionModel(BaseModel):
                              "before it is saved.")
 
     def load(self, path: str):
+        """
+        Loads the model from the given path
+        :param path: path and model name in modelzoo
+        """
         self.model_trained = True
         # TODO: fix path to modelzoo here
         self.model = pkl.load(path)
