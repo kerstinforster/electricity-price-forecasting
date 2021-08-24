93811f24e2e28b94c6b8017bea57c681149dcb17
ga87giv <ga87giv@mytum.de>
2021-08-24 23:44:48 +0200

    chore: update grid search file

--- grid_search.py
+++ grid_search.py
@@ -10,13 +10,14 @@ from src.grid_searcher import GridSearcher
 
 if __name__ == '__main__':
     # define a parameter grid for each model
-#    lstm_param_grid = {
-#        "model_name": ["lstm"],
-#        "window_size": [168],
-#        "gap": [0, 23, 167],  # Prediction horizons (hour, day, week)
-#        "num_layers": [1, 2],
-#        "hidden_layer_size": [128, 256]
-#    }
+    lstm_param_grid = {
+        "model_name": ["lstm"],
+        "window_size": [168],
+        "gap": [0, 23, 167],  # Prediction horizons (hour, day, week)
+        "num_layers": [1],
+        "hidden_layer_size": [128],
+        "epochs": [10]
+    }
 
     linear_param_grid = {
         "model_name": ["linear_regression"],
@@ -28,7 +29,8 @@ if __name__ == '__main__':
     }
 
     # list of dicts containing the parameter grid for the every model
-    all_model_param_grids = [linear_param_grid,
+    all_model_param_grids = [lstm_param_grid,
+                             linear_param_grid,
                              trivial_param_grid]
 
     # CREATE TRAIN AND TEST DATASET #
